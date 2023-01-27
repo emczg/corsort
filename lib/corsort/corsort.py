9da239709894d3d08085271c14dab9750b9e7377
balouf <fabien.mathieu@normalesup.org>
2023-01-27 16:02:12 +0000

    Deploying to gh-pages from  @ d73b90339f31c6c382b3308f2d244891ce0a9985 🚀

--- lib/corsort/corsort.py
+++ lib/corsort/corsort.py
@@ -179,6 +179,42 @@ class CorSort:
         return self.n_c_
 
 
+class CorSortBorda(CorSort):
+    """
+    Examples
+    --------
+
+    >>> np.random.seed(22)
+    >>> n_ = 15
+    >>> p = np.random.permutation(n_)
+    >>> c = CorSortBorda()
+    >>> c(p)
+    44
+    >>> entropy_bound(n_) # doctest: +ELLIPSIS
+    40.869...
+    >>> c.distances_ # doctest: +NORMALIZE_WHITESPACE
+    [55, 42, 51, 49, 49, 48, 40, 39, 33, 29, 29, 28, 28, 28, 28, 26, 26, 21, 20, 16, 14, 11, 10, 9, 8, 7,
+     8, 7, 6, 7, 7, 7, 7, 5, 5, 4, 4, 4, 3, 2, 3, 2, 1, 0, 0]
+    """
+
+    def next_compare(self):
+        while True:
+            gain_matrix = -np.abs(self.pos_[np.newaxis, :] - self.pos_[:, np.newaxis])
+            arg = None
+            gain = None
+            for i in range(self.n_):
+                for j in range(i + 1, self.n_):
+                    if i not in self.de_[j] and i not in self.an_[j]:
+                        ng = gain_matrix[i, j]
+                        if gain is None or ng > gain:
+                            arg = (i, j)
+                            gain = ng
+            if arg is not None:
+                yield arg
+            else:
+                break
+
+
 class CorSortLexi(CorSort):
     """
     Examples
@@ -209,7 +245,7 @@ class CorSortLexi(CorSort):
 
         Returns
         -------
-        :class:`int`
+        :class:`tuple`
             Potential gain if we compare i and j and find that i<j
         """
         gain = 0
@@ -233,7 +269,7 @@ class CorSortLexi(CorSort):
 
         Returns
         -------
-        :class:`int`
+        :class:`tuple`
             Ensured gain if i and j are compared.
 
         """
