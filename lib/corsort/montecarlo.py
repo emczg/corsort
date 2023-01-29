7d017f56e747da53288638898c65ca73db3ee2dc
balouf <fabien.mathieu@normalesup.org>
2023-01-29 19:56:23 +0000

    Deploying to gh-pages from  @ 10f0a572c5a2baa65a88b1a414277440ed50e04c 🚀

--- lib/corsort/montecarlo.py
+++ lib/corsort/montecarlo.py
@@ -1,12 +1,17 @@
 import numpy as np
 from multiprocess.pool import Pool
+from collections import defaultdict
+from functools import partial
 from tqdm import tqdm
 
 
-def print_res(sort_list, n_list, means, stds):
-    for j, n in enumerate(n_list):
-        for i, sort in enumerate(sort_list):
-            print(f"n={n}, {sort.__name__}: mean={means[i, j]:.2f}, std={stds[i, j]:.2f}")
+def print_res(res):
+    for name, di in res.items():
+        for n, v in di.items():
+            t = v['time']
+            m = np.mean(t)
+            s = np.std(t)
+            print(f"n={n}, {name}: mean={m:.2f}, std={s:.2f}")
 
 
 def evaluate(sort_list, n_list, nt, pool=None):
@@ -34,29 +39,29 @@ def evaluate(sort_list, n_list, nt, pool=None):
     With evaluate corsort and quicksort using a Pool:
 
     >>> with Pool() as p:
-    ...     means, stds = evaluate(sort_list, n_list, nt=n_t, pool=p)
+    ...     res = evaluate(sort_list, n_list, nt=n_t, pool=p)
     Evaluate quicksort for n = 10
     Evaluate corsort for n = 10
     Evaluate quicksort for n = 15
     Evaluate corsort for n = 15
-    >>> print_res(sort_list, n_list, means, stds)
+    >>> print_res(res)
     n=10, quicksort: mean=24.05, std=3.52
-    n=10, corsort: mean=22.12, std=0.89
     n=15, quicksort: mean=46.72, std=6.90
+    n=10, corsort: mean=22.12, std=0.89
     n=15, corsort: mean=40.94, std=1.34
 
     Same without the pool:
 
     >>> np.random.seed(42)
-    >>> means, stds = evaluate(sort_list, n_list, nt=n_t)
+    >>> res = evaluate(sort_list, n_list, nt=n_t)
     Evaluate quicksort for n = 10
     Evaluate corsort for n = 10
     Evaluate quicksort for n = 15
     Evaluate corsort for n = 15
-    >>> print_res(sort_list, n_list, means, stds)
+    >>> print_res(res)
     n=10, quicksort: mean=24.05, std=3.52
-    n=10, corsort: mean=22.12, std=0.89
     n=15, quicksort: mean=46.72, std=6.90
+    n=10, corsort: mean=22.12, std=0.89
     n=15, corsort: mean=40.94, std=1.34
 
     Bound (loose, not exact):
@@ -65,10 +70,59 @@ def evaluate(sort_list, n_list, nt, pool=None):
     Bound for n=10: 22.11
     Bound for n=15: 40.87
     """
-    means = np.zeros((len(sort_list), len(n_list)))
-    stds = np.zeros((len(sort_list), len(n_list)))
-    for j, n in enumerate(n_list):
-        for i, sort in enumerate(sort_list):
+    res = defaultdict(dict)
+    for n in n_list:
+        for sort in sort_list:
+            print(f"Evaluate comparisons of {sort.__name__} for n = {n}")
+            convs = np.zeros(nt, dtype=int)
+            distances = []
+            if pool is not None:
+                for k, cd in enumerate(pool.imap_unordered(sort,
+                                                           tqdm([np.random.permutation(n)
+                                                                 for _ in range(nt)]))):
+                    convs[k] = cd[0]
+                    distances.append(cd[1])
+            else:
+                for k in tqdm(range(nt)):
+                    cd = sort(np.random.permutation(n))
+                    convs[k] = cd[0]
+                    distances.append(cd[1])
+            max_d = max(len(d) for d in distances)
+            dist_array = np.zeros((nt, max_d), dtype=int)
+            for i, dist in enumerate(distances):
+                dist_array[i, :len(dist)] = dist
+            res[sort.__name__][n] = {'time': convs, 'distance': dist_array}
+    return res
+
+
+def evaluate_convergence(sort_list, n, nt, pool=None):
+    res = dict()
+    for raw_sort in sort_list:
+        name = raw_sort.__name__
+        sort = partial(raw_sort, compute_history=True)
+        print(f"Evaluate convergence of {name} for n = {n}")
+        distances = []
+        if pool is not None:
+            for k, cd in enumerate(pool.imap_unordered(sort,
+                                                       tqdm([np.random.permutation(n)
+                                                             for _ in range(nt)]))):
+                distances.append(cd[1])
+        else:
+            for k in tqdm(range(nt)):
+                cd = sort(np.random.permutation(n))
+                distances.append(cd[1])
+        max_d = max(len(d) for d in distances)
+        dist_array = np.zeros((nt, max_d), dtype=int)
+        for i, dist in enumerate(distances):
+            dist_array[i, :len(dist)] = dist
+        res[name] = dist_array
+    return res
+
+
+def evaluate_comparisons(sort_list, n_list, nt, pool=None):
+    res = defaultdict(dict)
+    for n in n_list:
+        for sort in sort_list:
             print(f"Evaluate {sort.__name__} for n = {n}")
             convs = np.zeros(nt, dtype=int)
             if pool is not None:
@@ -78,6 +132,8 @@ def evaluate(sort_list, n_list, nt, pool=None):
                     convs[k] = cd[0]
             else:
                 for k in tqdm(range(nt)):
-                    convs[k] = sort(np.random.permutation(n))[0]
-            means[i, j], stds[i, j] = np.mean(convs), np.std(convs)
-    return means, stds
+                    cd = sort(np.random.permutation(n))
+                    convs[k] = cd[0]
+            res[sort.__name__][n] = convs
+    return res
+
