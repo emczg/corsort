e5311508db68fd13094bc69eaf36cb6b7315be1e
fabien <fabien.mathieu@nokia-bell-labs.com>
2023-01-29 09:18:44 +0100

    Enter numba

--- corsort/__init__.py
+++ corsort/__init__.py
@@ -8,6 +8,7 @@ __version__ = '0.1.0'
 from corsort.corsort import corsort, entropy_bound
 from corsort.quicksort import quicksort
 from corsort.montecarlo import evaluate
+from corsort.distance_to_sorted_array import distance_to_sorted_array
 from corsort.sub_package_1.my_class_1 import MyClass1
 from corsort.sub_package_2.my_class_2 import MyClass2
 from corsort.sub_package_2.my_class_3 import MyClass3
