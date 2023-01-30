dad023b07ff9b46ba26ee8938ffd4b52bca54b77
francois-durand <fradurandpub@gmail.com>
2023-01-30 16:17:13 +0000

    Deploying to gh-pages from  @ aa767f8248b2833b8a5e3303fcc11545b0f0ddab 🚀

--- lib/corsort/CorSort.py
+++ lib/corsort/CorSort.py
@@ -5,79 +5,128 @@ from corsort import distance_to_sorted_array
 
 class CorSort:
     """
+    CorSort.
+
+    Parameters
+    ----------
+    compute_history: :class:`bool`
+        If True, then compute the history of the distance to the sorted array.
+
     Attributes
     ----------
-
     n_: :class:`int`:
-        Number of items
+        Number of items in the list.
     perm_: :class:`~numpy.ndarray`
-        Input permutation
-    an_: :class:`list` of :class:`set`
-        Ancestors
-    de_: :class:`list` of :class:`set`
-        Descendants
-    distances_: :class:`list` of :class:`int`
-        KT distance to complete sort
-    n_c_: :class:`int`
+        Input permutation.
+    sets_ancestors_: :class:`list` of :class:`set`
+        For each index (in the original list), set of its ancestors including itself, i.e. items for which we
+        know they are greater or equal.
+    sets_descendants: :class:`list` of :class:`set`
+        For each index (in the original list), set of its descendants including itself, i.e. items for which we
+        know they are lower or equal.
+    history_distances_: :class:`list` of :class:`int`
+        History of the kendall-tau distance to the sorted list.
+    n_comparisons_: :class:`int`
         Number of comparison performed.
+    position_estimates_: :class:`list` of :class:`float`
+        For each index (in the original list), its position estimate in the sorted list.
+        Note that a position of 0 means the start of the sorted list, i.e. smallest element, whereas
+        a position of `n - 1` means the end of the sorted list, i.e. the greatest element. In other
+        words, the position estimates are the Borda scores.
     """
-    def __init__(self):
+    def __init__(self, compute_history=False):
+        # Options
+        self.compute_history = compute_history
+        # Computed attributes
         self.n_ = None
         self.perm_ = None
-        self.an_ = None
-        self.de_ = None
-        self.distances_ = None
-        self.n_c_ = None
-        self.pos_ = None
+        self.sets_ancestors_ = None
+        self.sets_descendants = None
+        self.history_distances_ = None
+        self.n_comparisons_ = None
+        self.position_estimates_ = None
 
-    def update_pos(self):
+    def update_position_estimates(self):
         """
-        Update estimated position of each element.
+        Update position estimate of each item.
 
-        Returns
-        -------
-        None
+        Examples
+        --------
+            >>> corsort = CorSort()
+            >>> corsort.n_ = 4
+            >>> corsort.sets_ancestors_ = [{1, 2, 3}, {}, {}, {1}]
+            >>> corsort.sets_descendants = [{}, {0, 2, 3}, {}, {2}]
+            >>> corsort.update_position_estimates()
+            >>> corsort.position_estimates_
+            array([0. , 3. , 1.5, 1.5])
         """
-        self.pos_ = np.array([(len(self.de_[i]) - 1 + self.n_ - len(self.an_[i])) / 2 for i in range(self.n_)])
+        self.position_estimates_ = np.array([
+            (len(self.sets_descendants[i]) - 1 + self.n_ - len(self.sets_ancestors_[i])) / 2
+            for i in range(self.n_)
+        ])
 
     def test_i_lt_j(self, i, j):
         """
+        Test whether perm[i] < perm[j].
 
         Parameters
         ----------
         i: :class:`int`
-            First index
+            First index.
         j: :class:`int`
-            Second index
+            Second index.
 
         Returns
         -------
         :class:`bool`
-            The comparison between the two indexed elements.
+            True if item of index `i` is lower than item of index `j`.
 
+        Examples
+        --------
+            >>> corsort = CorSort()
+            >>> corsort.perm_ = ['b', 'a']
+            >>> corsort.test_i_lt_j(0, 1)
+            False
         """
         return self.perm_[i] < self.perm_[j]
 
     def apply_i_lt_j(self, i, j):
         """
-        Assuming i<j, updates the poset accordingly.
+        Assuming perm[i] < perm[j], updates the poset accordingly.
 
         Parameters
         ----------
         i: :class:`int`
-            Index of the small element.
+            Index of the small item.
         j: :class:`int`
-            Index of the big element
+            Index of the big item.
 
-        Returns
-        -------
-        None
+        Examples
+        --------
+            >>> corsort = CorSort()
+            >>> corsort.n_ = 4
+
+        Assume that we know perm[0] < perm[1], and perm[2] < perm[3]:
+
+            >>> corsort.sets_ancestors_ = [{1}, {}, {3}, {}]
+            >>> corsort.sets_descendants = [{}, {0}, {}, {2}]
+
+        Assume we learn that perm[1] < perm[2]:
+
+            >>> corsort.apply_i_lt_j(1, 2)
+            >>> corsort.sets_ancestors_
+            [{1, 3}, {}, {3}, {}]
+            >>> corsort.sets_descendants
+            [{}, {0}, {}, {0, 2}]
+
+        But that is a bug, because at that stage we should know the full order.
         """
-        for jj in self.an_[j]:
-            self.de_[jj] |= self.de_[i]
-        for ii in self.de_[i]:
-            self.an_[ii] |= self.an_[j]
-        self.update_pos()
+        # TODO: solve this bug.
+        for ancestor_of_j in self.sets_ancestors_[j]:
+            self.sets_descendants[ancestor_of_j] |= self.sets_descendants[i]
+        for descendant_of_i in self.sets_descendants[i]:
+            self.sets_ancestors_[descendant_of_i] |= self.sets_ancestors_[j]
+        self.update_position_estimates()
 
     def compare(self, i, j):
         """
@@ -102,11 +151,18 @@ class CorSort:
     def next_compare(self):
         """
         Iterator of pairwise comparisons to make.
+
+        Returns
+        -------
+        iterable
+            An iterator of pairwise comparisons to make.
         """
         raise NotImplementedError
 
     def __call__(self, perm):
         """
+        Sort.
+
         Parameters
         ----------
         perm: :class:`numpy.ndarray`
@@ -121,13 +177,15 @@ class CorSort:
             perm = np.array(perm)
         self.n_ = len(perm)
         self.perm_ = perm
-        self.an_ = [{i} for i in range(self.n_)]
-        self.de_ = [{i} for i in range(self.n_)]
-        self.distances_ = [distance_to_sorted_array(self.perm_)]
-        self.update_pos()
+        self.sets_ancestors_ = [{i} for i in range(self.n_)]
+        self.sets_descendants = [{i} for i in range(self.n_)]
+        self.n_comparisons_ = 0
+        self.history_distances_ = [distance_to_sorted_array(self.perm_)] if self.compute_history else []
+        self.update_position_estimates()
         for c in self.next_compare():
             self.compare(*c)
-            self.distances_.append(distance_to_sorted_array(
-                self.perm_[np.argsort(self.pos_)]))
-        self.n_c_ = len(self.distances_) - 1
-        return self.n_c_
+            self.n_comparisons_ += 1
+            if self.compute_history:
+                self.history_distances_.append(
+                    distance_to_sorted_array(self.perm_[np.argsort(self.position_estimates_)]))
+        return self.n_comparisons_
--- lib/corsort/CorSortDelegate.py
+++ lib/corsort/CorSortDelegate.py
@@ -16,6 +16,7 @@ class CorSortDelegate(CorSort):
     --------
         >>> cor_sort_quicksort = CorSortDelegate(quicksort)
         >>> cor_sort_quicksort(['e', 'b', 'a', 'c', 'd'])
+        8
         >>> cor_sort_quicksort.history_comparisons
         [(1, 0), (2, 0), (3, 0), (4, 0), (2, 1), (3, 1), (4, 1), (4, 3)]
     """
@@ -26,8 +27,6 @@ class CorSortDelegate(CorSort):
         self.sort_and_record = record_comparisons(self.sort, self.history_comparisons)
         super().__init__()
 
-    def __call__(self, perm):
-        self.sort_and_record(perm)  # Updates self.history_comparisons
-
     def next_compare(self):
+        self.sort_and_record(self.perm_)  # Updates self.history_comparisons
         return self.history_comparisons
