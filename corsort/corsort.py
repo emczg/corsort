92cbd042036c2dc102e0033c67ba421d1bd9cc02
fabien <fabien.mathieu@nokia-bell-labs.com>
2023-01-30 13:34:49 +0100

    Improved corsort

--- corsort/corsort.py
+++ corsort/corsort.py
@@ -6,30 +6,34 @@ from corsort.distance_to_sorted_array import distance_to_sorted_array
 @njit
 def jit_corsort(perm):
     n = len(perm)
-    an = np.eye(n, dtype=np.bool_)
-    pos = np.zeros(n, dtype = np.int_)
+    leq = np.eye(n, dtype=np.bool_)
+    pos = np.zeros(n, dtype=np.int_)
+    info = np.zeros(n, dtype=np.int_)
     distances = [perm[np.argsort(pos)]]
     while True:
-        gain, ij = 0, (0,0)
-        for i in range(n):
-            for j in range(i+1, n):
-                if (not an[i, j]) and (not an[j, i]):
-                    pij = 2*n - abs(pos[i]-pos[j])
-                    if pij > gain:
-                        gain = pij
-                        ij = (i, j)
-        i, j = ij
-        if gain == 0:
+        diff = n
+        xp = 0
+        for ii in range(n):
+            for jj in range(ii+1, n):
+                if (not leq[ii, jj]) and (not leq[jj, ii]):
+                    diff_ij = abs(pos[ii]-pos[jj])
+                    xp_ij = info[ii] + info[jj]
+                    if (diff_ij < diff) or (diff_ij==diff and xp_ij>xp):
+                        diff = diff_ij
+                        xp = xp_ij
+                        i, j = ii, jj
+        if diff == n:
             break
         if perm[i] > perm[j]:
             i, j = j, i
         for ii in range(n):
             for jj in range(n):
-                if an[ii, i] and an[j, jj]:
-                    if not an[ii, jj]:
-                        an[ii, jj] = True
-                        pos[ii] -= 1
-                        pos[jj] += 1
+                if leq[ii, i] and leq[j, jj] and (not leq[ii, jj]):
+                    leq[ii, jj] = True
+                    info[ii] += 1
+                    info[jj] += 1
+                    pos[ii] -= 1
+                    pos[jj] += 1
         distances.append(perm[np.argsort(pos)])
     return distances
 
@@ -58,13 +62,12 @@ def corsort(perm, compute_history=False):
     >>> p = np.random.permutation(n)
     >>> d, dists =corsort(p, compute_history=True)
     >>> d
-    44
+    43
     >>> entropy_bound(n) # doctest: +ELLIPSIS
     40.869...
     >>> dists # doctest: +NORMALIZE_WHITESPACE
-    [55, 42, 51, 49, 49, 48, 40, 39, 33, 29, 29, 28, 28, 28, 28, 26, 26, 21, 20, 16, 14, 11, 10, 9, 8, 7,
-     8, 7, 6, 7, 7, 7, 7, 5, 5, 4, 4, 4, 3, 2, 3, 2, 1, 0, 0]
-
+    [55, 42, 51, 49, 49, 48, 40, 39, 33, 29, 29, 29, 28, 28, 28, 26, 26, 21, 20, 16, 14, 11, 10, 9, 8, 10,
+    8, 7, 6, 7, 5, 4, 4, 5, 4, 3, 4, 3, 3, 2, 2, 0, 1, 0]
     """
     states = jit_corsort(perm)
     history = [distance_to_sorted_array(state) for state in states] if compute_history else []
