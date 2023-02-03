caeabb802078374e2eb89cc6a1feeeec603ab83e
francois-durand <fradurandpub@gmail.com>
2023-02-03 11:56:39 +0100

    JitSortGeneric

---
+++ corsort/JitSortGeneric.py
@@ -0,0 +1,46 @@
+import numpy as np
+from corsort.distance_to_sorted_array import distance_to_sorted_array
+from corsort.SortQuick import SortQuick
+from corsort.scorer_spaced import scorer_spaced
+
+
+class JitSortGeneric:
+    """
+    Examples
+    --------
+        >>> my_sort = SortQuick(compute_history=False)
+        >>> jit_sort = JitSortGeneric(scorer=scorer_spaced, sort=my_sort, compute_history=False)
+        >>> my_xs = np.array([4, 1, 7, 6, 0, 8, 2, 3, 5])
+        >>> jit_sort(my_xs).n_comparisons_
+        17
+    """
+
+    def __init__(self, scorer, sort, compute_history=False):
+        # Parameters
+        self.scorer = scorer
+        self.sort = sort
+        self.compute_history = compute_history
+        # Computed values
+        self.n_ = None
+        self.perm_ = None
+        self.n_comparisons_ = None
+        self.history_distances_ = None
+        self.history_comparisons_ = None
+
+    def __call__(self, perm):
+        if isinstance(perm, list):
+            perm = np.array(perm)
+        self.n_ = len(perm)
+        self.perm_ = perm
+        self.sort(perm)
+        self.history_comparisons_ = self.sort.history_comparisons_
+        downs = np.array([c[0] for c in self.sort.history_comparisons_])
+        ups = np.array([c[1] for c in self.sort.history_comparisons_])
+        states = self.scorer(self.n_, downs, ups)
+        self.n_comparisons_ = states.shape[0] - 1
+        if self.compute_history:
+            self.history_distances_ = [distance_to_sorted_array(self.perm_[np.argsort(state)])
+                                       for state in states]
+        else:
+            self.history_distances_ = []
+        return self
