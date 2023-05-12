5ade91626a7f39a6e99766c1e6144d4fba357aa7
francois-durand <fradurandpub@gmail.com>
2023-05-12 14:02:25 +0000

    Deploying to gh-pages from  @ f3db6c562cf60d21ce353c8aae709684fb051579 🚀

--- lib/corsort/sort_merge_bfs.py
+++ lib/corsort/sort_merge_bfs.py
@@ -2,6 +2,7 @@ import numpy as np
 from corsort.sort import Sort
 from corsort.distance_to_sorted_array import distance_to_sorted_array
 from corsort.merge import merge
+from corsort.split_pointer_lists import split_pointer_lists
 
 
 class SortMergeBfs(Sort):
@@ -43,74 +44,6 @@ class SortMergeBfs(Sort):
         return self.perm_[self.sorted_indices_]
 
 
-def _sub_step(list_indices):
-    """
-    Compute the indices of the boundaries for the next sub-step of BFS merge sort.
-
-    Parameters
-    ----------
-    list_indices: :class:`~numpy.ndarray`
-        List of indices for the current step.
-
-    Returns
-    -------
-    :class:`~numpy.ndarray`
-        List of indices for the sub-step.
-
-    Examples
-    --------
-        >>> my_indices = np.array([0, 9])
-        >>> my_indices = _sub_step(my_indices)
-        >>> my_indices
-        array([0, 4, 9])
-        >>> my_indices = _sub_step(my_indices)
-        >>> my_indices
-        array([0, 2, 4, 6, 9])
-        >>> my_indices = _sub_step(my_indices)
-        >>> my_indices
-        array([0, 1, 2, 3, 4, 5, 6, 7, 9])
-        >>> my_indices = _sub_step(my_indices)
-        >>> my_indices
-        array([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9])
-    """
-    list_indices_sub_step = []
-    for i, j in zip(list_indices[:-1], list_indices[1:]):
-        list_indices_sub_step.append(i)
-        list_indices_sub_step.append((i + j) // 2)
-    list_indices_sub_step.append(list_indices[-1])
-    return np.array(list_indices_sub_step)
-
-
-def _lists_indices_steps(n):
-    """
-    Compute the indices of the boundaries for all the steps of BFS merge sort.
-
-    Parameters
-    ----------
-    n: :class:`integer`
-        Size of the list.
-
-    Returns
-    -------
-    :class:`list` of :class:`~numpy.ndarray`
-        For each step, list of indices for the step.
-
-    Examples
-    --------
-        >>> _lists_indices_steps(n=9)  # doctest: +NORMALIZE_WHITESPACE
-        [array([0, 4, 9]),
-        array([0, 2, 4, 6, 9]),
-        array([0, 1, 2, 3, 4, 5, 6, 7, 9]),
-        array([0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 8, 9])]
-    """
-    list_indices = np.array([0, n])
-    result = []
-    while np.max(list_indices[1:] - list_indices[:-1]) > 1:
-        list_indices = _sub_step(list_indices)
-        result.append(list_indices)
-    return result
-
-
 def _merge_sort_bfs(collection, lt=None):
     """
 
@@ -143,7 +76,7 @@ def _merge_sort_bfs(collection, lt=None):
         array([0, 1, 2, 3, 4, 5, 6, 7, 8])
     """
     n = len(collection)
-    lists_indices = _lists_indices_steps(n)
-    for list_indices in lists_indices[::-1]:
-        for i in range(0, len(list_indices) - 1, 2):
-            merge(collection, list_indices[i], list_indices[i+1], list_indices[i+2], lt=lt)
+    _split_pointer_lists = split_pointer_lists(n)
+    for split_pointer_list in _split_pointer_lists[::-1]:
+        for i in range(0, len(split_pointer_list) - 1, 2):
+            merge(collection, split_pointer_list[i], split_pointer_list[i+1], split_pointer_list[i+2], lt=lt)
